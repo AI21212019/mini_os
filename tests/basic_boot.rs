0535c0baacc84af2c9f211f23381ab88ca92e2a1
Hector <hector.quijada2121@gmail.com>
2022-03-09 22:20:37 -0500

    complete integration test

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,25 +7,18 @@
 use core::panic::PanicInfo;
 use mini_os::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     mini_os::test_panic_handler(info)
